6d40aa665c03530f3839e1ab5f11258f85cdb419
DESKTOP-EB88TB7\TUANKIET <tuankietlenguyen2709@gmail.com>
2023-12-08 21:56:00 +0700

    fix seeder 2

--- src/database/seeders/TagSeeder.php
+++ src/database/seeders/TagSeeder.php
@@ -44,26 +44,37 @@ class TagSeeder extends Seeder
             ]);
         }
 
-        $productId = 1;
-        $tagId = 1;
+        $maxProductId = 20;
+        $maxTagId = 10;
 
-        for ($i = 0; $i < 55; $i++) {
-            $user = rand(1,10);
-            if ($tagId == 10) {
-                $tagId = 1;
-                $productId ++;
+        for ($productId = 1; $productId <= $maxProductId; $productId++) {
+            // Random number of tags for each product (between 1 and 4)
+            $numberOfTags = rand(1, 4);
+
+            // Array to keep track of assigned tags to avoid duplicates
+            $assignedTags = [];
+
+            for ($j = 0; $j < $numberOfTags; $j++) {
+                do {
+                    // Random tagId
+                    $tagId = rand(1, $maxTagId);
+                } while (in_array($tagId, $assignedTags)); // Ensure unique tagId for each product
+
+                $user = rand(1, 10);
+
+                // Insert product_tag into the database
+                DB::table('product_tags')->insert([
+                    'product_id' => $productId,
+                    'tag_id' => $tagId,
+                    'created_by' => $user,
+                    'updated_by' => $user,
+                    'created_at' => date('Y-m-d H:i:s', strtotime('-' . rand(1, 5) . ' years')),
+                    'updated_at' => date('Y-m-d H:i:s', strtotime('-' . rand(1, 5) . ' years +1 day')),
+                ]);
+
+                // Add the tag to the assignedTags array to avoid duplication
+                $assignedTags[] = $tagId;
             }
-
-            DB::table('product_tags')->insert([
-                'product_id' => $productId,
-                'tag_id' => $tagId,
-                'created_by' => $user,
-                'updated_by' => $user,
-                'created_at' => date('Y-m-d H:i:s', strtotime('-' . rand(1, 5) . ' years')),
-                'updated_at' => date('Y-m-d H:i:s', strtotime('-' . rand(1, 5) . ' years +1 day')),
-            ]);
-
-            $tagId ++;
         }
     }
 }
